0d24fbb8453611a65884a9f46c2c8105883ec3ac
cloudy4next <jahngir7200@live.com>
2023-02-26 21:32:12 +0600

    Feat:acl

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,31 +2,51 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>
         {{ trans('backpack::base.dashboard') }}</a></li>
 
-<li class="nav-item nav-dropdown">
-    <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> Authentication</a>
-    <ul class="nav-dropdown-items">
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i>
-                <span>Users</span></a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i
-                    class="nav-icon la la-id-badge"></i> <span>Roles</span></a></li>
-        <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i
-                    class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
-    </ul>
-</li>
+@if (backpack_user()->hasRole('Super Admin'))
+    <li class="nav-item nav-dropdown">
+        <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-users"></i> Authentication</a>
+        <ul class="nav-dropdown-items">
+            <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i
+                        class="nav-icon la la-user"></i>
+                    <span>Users</span></a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i
+                        class="nav-icon la la-id-badge"></i> <span>Roles</span></a></li>
+            <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i
+                        class="nav-icon la la-key"></i> <span>Permissions</span></a></li>
+        </ul>
+    </li>
+@endif
+@if (backpack_user()->hasPermissionTo('Category'))
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i
+                class='nav-icon la la-question'></i>
+            Categories</a></li>
+@endif
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('category') }}'><i class='nav-icon la la-question'></i>
-        Categories</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('expense') }}'><i class='nav-icon la la-question'></i>
-        Expenses</a></li>
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('profit') }}'><i class='nav-icon la la-question'></i>
-        Daily Upfront</a></li>
+@if (backpack_user()->hasPermissionTo('Expense'))
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('expense') }}'><i
+                class='nav-icon la la-question'></i>
+            Expenses</a></li>
+@endif
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('salary') }}'><i class='nav-icon la la-question'></i>
-        Salaries</a></li>
+@if (backpack_user()->hasPermissionTo('Profit'))
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('profit') }}'><i class='nav-icon la la-question'></i>
+            Daily Upfront</a></li>
+@endif
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('inventment') }}'><i class='nav-icon la la-question'></i>
-        Investments</a></li>
 
-<li class='nav-item'><a class='nav-link' href="{{ route('audit.log.index') }}"><i class='nav-icon la la-memory'></i>
-        <span>Activity Log</span></a></li>
+@if (backpack_user()->hasRole('Super Admin'))
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('salary') }}'><i class='nav-icon la la-question'></i>
+            Salaries</a></li>
+
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('salary') }}'><i class='nav-icon la la-question'></i>
+            Salaries</a></li>
+
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('inventment') }}'><i
+                class='nav-icon la la-question'></i>
+            Investments</a></li>
+
+    <li class='nav-item'><a class='nav-link' href="{{ route('audit.log.index') }}"><i
+                class='nav-icon la la-memory'></i>
+            <span>Activity Log</span></a></li>
+@endif
